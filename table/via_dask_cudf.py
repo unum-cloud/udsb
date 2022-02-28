04c00d683983aa3e0dea74e1dd14281c02c5964e
Ashot Vardanian <1983160+ashvardanian@users.noreply.github.com>
2022-02-28 18:20:07 +0000

    Progress with Dask CuDF

--- table/via_dask_cudf.py
+++ table/via_dask_cudf.py
@@ -1,15 +1,14 @@
-import os
-import yaml
+from typing import List
 
 import dask
 import dask.distributed  # Imports new config values
 import dask_cudf
 import dask.config
 
-from dask_cudf import LocalCUDACluster
+from dask_cuda import LocalCUDACluster
 from dask.distributed import Client
 
-from via_pandas import ViaPandas
+from via_pandas import ViaPandas, taxi_rides_paths
 
 
 class ViaDaskCuDF(ViaPandas):
@@ -20,18 +19,32 @@ class ViaDaskCuDF(ViaPandas):
         https://docs.rapids.ai/api/dask-cuda/nightly/examples/ucx.html
     """
 
-    def __init__(self, **kwargs) -> None:
+    def __init__(self, paths: List[str] = taxi_rides_paths()) -> None:
         # Dask loves spawning zombi processes:
         # https://docs.dask.org/en/stable/configuration.html
         # fn = os.path.join(os.path.dirname(__file__), 'dask_config.yml')
         # defaults = yaml.safe_load(open(fn).read())
         # dask.config.update_defaults(defaults)
         self.cluster = LocalCUDACluster(
-            # enable_nvlink=True,
-            # log_spilling=False,
+            # InfiniBand and UCX
+            # https://developer.nvidia.com/blog/high-performance-python-communication-with-ucx-py/
+            protocol="ucx",
+            enable_nvlink=True,
+            log_spilling=False,
+            memory_limit=None,
+
         )
         self.client = Client(self.cluster)
-        super().__init__(backend=dask_cudf, **kwargs)
+        self.backend = dask_cudf
+        files = [self.backend.read_parquet(p) for p in paths]
+        self.df = self.backend.concat(files, ignore_index=True)
+
+        # Passenger count can't be a `None`
+        # Passenger count can't be zero or negative
+        is_abnormal = self.client.compute(
+            self.df['passenger_count'].lt(1), sync=True)
+        self.df['passenger_count'] = self.client.compute(
+            self.df['passenger_count'].mask(is_abnormal, 1))
 
     def __del__(self):
         self.client.shutdown()
