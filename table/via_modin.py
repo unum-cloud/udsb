2b777be01b1a1c119619173cf10a2ed271d5bebd
Ashot Vardanian <1983160+ashvardanian@users.noreply.github.com>
2022-03-01 12:07:52 +0000

    Reduced Ray warnings in Modin

--- table/via_modin.py
+++ table/via_modin.py
@@ -1,3 +1,6 @@
+import os
+os.environ['RAY_DISABLE_IMPORT_WARNING'] = 1  # nopep8
+
 import ray
 import modin.pandas
 from modin.config import Engine
@@ -14,7 +17,7 @@ class ViaModin(ViaPandas):
         super().__init__(modin.pandas, **kwargs)
 
     def cleanup(self):
-        # Modin doesn't natively support masking and defaults ot Pandas.
+        # Modin doesn't natively support masking and defaults to Pandas.
         self.df['passenger_count'] = self.df['passenger_count'].mask(
             self.df['passenger_count'].lt(1), 1)
 
