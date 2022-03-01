08ddb1e1fc1e794a054a6fc72b3a6643cf0c7273
Ashot Vardanian <1983160+ashvardanian@users.noreply.github.com>
2022-03-01 12:08:27 +0000

    Skipping repeated benchmarks

--- table/benchmark.py
+++ table/benchmark.py
@@ -2,58 +2,58 @@
 import os
 import time
 from dataclasses import dataclass
-from typing import Generator, List
+from typing import Callable, Generator, List
+from xmlrpc.client import Boolean
 
 import fire
 import pandas as pd
 
 from via_pandas import taxi_rides_paths
 
+max_seconds = 10.0
+current_instance = None
+NoneType = type(None)
+
 
 @dataclass
 class Sample:
-    operation: str = ''
+    # Categorical properties of the sample
     backend: str = ''
+    operation: str = ''
+    size_bytes: int = 0
+    # Runtime results
     seconds: float = 0.0
     iterations: int = 0
-    size: int = 0
     error: str = ''
 
+    def __repr__(self) -> str:
+        suffix = self.error if len(
+            self.error) else f'{self.iterations} iterations'
+        return f'{self.backend}.{self.operation}({self.size_bytes}): {suffix}'
 
-def run_backend(class_: type, class_name: str, paths: List[str]) -> Generator[Sample, None, None]:
-    max_seconds = 10.0
-    instance = None
 
-    def parse():
-        nonlocal instance
-        instance = class_(paths=paths)
+@dataclass
+class Bench:
+    # Categorical properties of the sample
+    backend: str
+    operation: str
+    size_bytes: int
+    # Runtime callable
+    func: Callable[[], NoneType]
 
-    def q1():
-        nonlocal instance
-        return instance.query1()
+    def __repr__(self) -> str:
+        return f'{self.backend}.{self.operation}({self.size_bytes})'
 
-    def q2():
-        nonlocal instance
-        return instance.query2()
-
-    def q3():
-        nonlocal instance
-        return instance.query3()
-
-    def q4():
-        nonlocal instance
-        return instance.query4()
-
-    funcs = [parse, q1, q2, q3, q4]
-    for func in funcs:
+    def __call__(self) -> Sample:
         s = Sample()
-        s.operation = func.__name__
-        s.backend = class_name
+        s.operation = self.operation
+        s.backend = self.backend
+        s.size_bytes = self.size_bytes
 
         start = time.time()
         while True:
             try:
-                func()
+                self.func()
             except Exception as e:
                 s.error = str(e)
                 break
@@ -63,31 +63,81 @@ def run_backend(class_: type, class_name: str, paths: List[str]) -> Generator[Sa
             if s.seconds > max_seconds:
                 break
 
-        yield s
-
-    instance = None
+        return s
 
 
-def run_backends(backend_names: List[str],  paths: List[str]) -> Generator[Sample, None, None]:
+def benchmarks_for_backend(class_: type, class_name: str, paths: List[str]) -> Generator[Bench, None, None]:
+
+    def parse():
+        global current_instance
+        current_instance = class_(paths=paths)
+
+    def q1():
+        global current_instance
+        if not isinstance(current_instance, class_):
+            current_instance = class_(paths=paths)
+        return current_instance.query1()
+
+    def q2():
+        global current_instance
+        if not isinstance(current_instance, class_):
+            current_instance = class_(paths=paths)
+        return current_instance.query2()
+
+    def q3():
+        global current_instance
+        if not isinstance(current_instance, class_):
+            current_instance = class_(paths=paths)
+        return current_instance.query3()
+
+    def q4():
+        global current_instance
+        if not isinstance(current_instance, class_):
+            current_instance = class_(paths=paths)
+        return current_instance.query4()
+
+    funcs = [parse, q1, q2, q3, q4]
+    for func in funcs:
+
+        yield Bench(
+            operation=func.__name__,
+            backend=class_name,
+            size_bytes=None,
+            func=func,
+        )
+
+    global current_instance
+    current_instance = None
+
+
+def benchmarks_for_backends(backend_names: List[str],  paths: List[str]) -> Generator[Bench, None, None]:
 
     if 'pandas' in backend_names:
         from via_pandas import ViaPandas
-        yield from run_backend(ViaPandas, 'pandas', paths)
+        yield from benchmarks_for_backend(ViaPandas, 'pandas', paths)
 
     if 'modin' in backend_names:
         from via_modin import ViaModin
-        yield from run_backend(ViaModin, 'modin', paths)
+        yield from benchmarks_for_backend(ViaModin, 'modin', paths)
 
     if 'cudf' in backend_names:
         from via_cudf import ViaCuDF
-        yield from run_backend(ViaCuDF, 'cudf', paths)
+        yield from benchmarks_for_backend(ViaCuDF, 'cudf', paths)
 
     if 'dask_cudf' in backend_names:
         from via_dask_cudf import ViaDaskCuDF
-        yield from run_backend(ViaDaskCuDF, 'dask_cudf', paths)
+        yield from benchmarks_for_backend(ViaDaskCuDF, 'dask_cudf', paths)
+
+    if 'sqlite' in backend_names:
+        from via_sqlite import ViaSQLite
+        yield from benchmarks_for_backend(ViaSQLite, 'sqlite', paths)
+
+    if 'spark' in backend_names:
+        from via_spark import ViaPySpark
+        yield from benchmarks_for_backend(ViaPySpark, 'spark', paths)
 
 
-def run_backends_and_sizes(backend_names: List[str]) -> Generator[Sample, None, None]:
+def benchmarks_for_all_sizes(backend_names: List[str]) -> Generator[Bench, None, None]:
 
     # Prepare different dataset sizes
     all_paths = taxi_rides_paths()
@@ -97,9 +147,9 @@ def run_backends_and_sizes(backend_names: List[str]) -> Generator[Sample, None, 
     size_categories = [
         0.01,
         0.02,
-        0.04,
-        0.08,
-        0.16,
+        # 0.04,
+        # 0.08,
+        # 0.16,
         # 0.32,
         # 0.64,
         # 1.0,
@@ -113,10 +163,16 @@ def run_backends_and_sizes(backend_names: List[str]) -> Generator[Sample, None, 
             if part_size / total_size >= size_category:
                 break
 
-        for s in run_backends(backend_names, part_paths):
-            s.size = part_size
-            print(s)
+        for s in benchmarks_for_backends(backend_names, part_paths):
+            s.size_bytes = part_size
             yield s
+
+
+def list_contains_benchmark(samples: List[Sample], bench: Bench) -> bool:
+    for s in samples:
+        if s.backend == bench.backend and s.operation == bench.operation and s.size_bytes == bench.size_bytes:
+            return True
+    return False
 
 
 def main(backend_names: List[str] = [], filename: os.PathLike = 'benchmark.json'):
@@ -124,32 +180,39 @@ def main(backend_names: List[str] = [], filename: os.PathLike = 'benchmark.json'
     # Validate passed argument
     if backend_names is None or len(backend_names) == 0:
         backend_names = [
-            # 'pandas',
-            # 'modin',
-            # 'cudf',
-            'dask_cudf',
+            'pandas',
+            'modin',
+            'cudf',
+            'sqlite',
+            # 'dask_cudf',
         ]
     if isinstance(backend_names, str):
         backend_names = backend_names.split(',')
     backend_names = [n.lower() for n in backend_names]
 
-    # Benchmark and track results
+    # Retiieve previous results
     samples = []
+    if os.path.exists(filename):
+        samples = pd.read_json(filename, orient='records')
+        samples = [Sample(**s) for _, s in samples.iterrows()]
+
+    # Bench and track results
     try:
-        for x in run_backends_and_sizes(backend_names):
-            if len(x.error) == 0:
-                samples.append(x)
+        for bench in benchmarks_for_all_sizes(backend_names):
+            if list_contains_benchmark(samples, bench):
+                print('Skipping:', bench)
+                continue
+            print('Will run:', bench)
+            sample = bench()
+            if len(sample.error) == 0:
+                samples.append(sample)
+                print('-- finished:', sample)
     except KeyboardInterrupt:
         pass
 
     # Format into a table
     samples = [s.__dict__ for s in samples]
     samples = pd.DataFrame(samples)
-
-    # Merge with older results, if present
-    if os.path.exists(filename):
-        old_results = pd.read_json(filename, orient='records')
-        samples = pd.concat([old_results, samples], ignore_index=True)
     samples.to_json(filename, orient='records')
 
 
