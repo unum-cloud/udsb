c3b72256cc874ffc3225f45222f8441c43a5db3f
Davit Vardanyan <78792753+davvard@users.noreply.github.com>
2022-03-21 09:12:22 +0000

    Add new datasets for ensuring slow size growth

--- network/preprocess.py
+++ network/preprocess.py
@@ -9,9 +9,11 @@ dataset_urls = {
     'butterfly_labels': 'https://snap.stanford.edu/biodata/datasets/10029/files/SS-Butterfly_labels.tsv.gz',  # 5 KB
     'bitcoin_alpha': 'https://snap.stanford.edu/data/soc-sign-bitcoinalpha.csv.gz',  # 503 KB
     'facebook': 'https://snap.stanford.edu/data/facebook_combined.txt.gz',  # 854 KB
-    'wiki_vote' : 'https://snap.stanford.edu/data/wiki-Vote.txt.gz', # 1.1 MB
-    'twitter' : 'https://snap.stanford.edu/data/twitter_combined.txt.gz', # 44.6 MB
-    'wiki_topcasts' : 'https://snap.stanford.edu/data/wiki-topcats.txt.gz', # 422 MB
+    'wiki_vote': 'https://snap.stanford.edu/data/wiki-Vote.txt.gz',  # 1.1 MB
+    'enron': 'https://snap.stanford.edu/data/email-Enron.txt.gz',  # 4 MB
+    'slashdot': 'https://snap.stanford.edu/data/soc-Slashdot0811.txt.gz',  # 10.7 MB
+    'twitter': 'https://snap.stanford.edu/data/twitter_combined.txt.gz',  # 44.6 MB
+    'wiki_topcasts': 'https://snap.stanford.edu/data/wiki-topcats.txt.gz',  # 422 MB
     'live_journal': 'https://snap.stanford.edu/data/soc-LiveJournal1.txt.gz',  # 1 GB
     'orkut': 'https://snap.stanford.edu/data/bigdata/communities/com-orkut.ungraph.txt.gz',  # 1.7 GB
 }
@@ -43,18 +45,18 @@ def download_datasets():
                 progress_bar.update(len(data))
                 file.write(data)
         progress_bar.close()
-        
+
         # Cleaning
-        if name in ['facebook','twitter','wiki_topcasts']:
+        if name in ['facebook', 'twitter', 'wiki_topcasts']:
             df = pd.read_csv(path, compression='gzip',
-                            sep=' ', header=0, skiprows=None)
+                             sep=' ', header=0, skiprows=None)
         elif name == 'bitcoin_alpha':
             df = pd.read_csv(path, compression='gzip',
-                            sep=',', header=0, skiprows=None)
+                             sep=',', header=0, skiprows=None)
         else:
             df = pd.read_csv(path, compression='gzip',
-                            sep='\t', header=0,comment='#', skiprows=None)
+                             sep='\t', header=0, comment='#', skiprows=None)
         df = df.iloc[:, 0:2]
         df.to_csv(os.path.join(save_path, name+'.txt'), sep=' ',
-                index=False, compression=None, header=False)
+                  index=False, compression=None, header=False)
         os.remove(path)
